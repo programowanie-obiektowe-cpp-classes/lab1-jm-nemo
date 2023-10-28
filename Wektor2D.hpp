da42f7c5cc4d49fbb300d1131effe7152d6ef080
jm-nemo <93876069+jm-nemo@users.noreply.github.com>
2023-10-28 21:49:23 +0200

    Update_3_Wektor2D.hpp

--- Wektor2D.hpp
+++ Wektor2D.hpp
@@ -1,14 +1,27 @@
 class Wektor2D
 {
+private:
+    double x;
+    double y;
+
 public:
-    Wektor2D() : x(0), y(0) {}
-    Wektor2D(double a, double b) : x(a), y(b)
+    Wektor2D(double xVal, double yVal) : x(xVal), y(yVal) {}
 
-    double getX() const { return x; }
-    double getY() const { return y; }
+    double getX() const {
+        return x;
+    }
 
-    void setX(double a) { x = a; }
-    void setY(double b) { y = b; }
+    double getY() const {
+        return y;
+    }
+
+    void setX(double newX) {
+        x = newX;
+    }
+
+    void setY(double newY) {
+        y = newY;
+    }
 
     Wektor2D operator+(const Wektor2D& other) const {
         Wektor2D result;
@@ -20,8 +33,4 @@ public:
     double operator*(const Wektor2D& other) const {
         return x * other.x + y * other.y;
     }
-
-private:
-    double x;
-    double y;
 };
